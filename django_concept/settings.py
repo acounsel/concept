f758772af78faccd19b7a1a21d6096f0ce996f50
Samer <sarrabi@gmail.com>
2020-04-24 19:04:58 -0700

    remove import

--- django_concept/settings.py
+++ django_concept/settings.py
@@ -1,8 +1,6 @@
 import django_heroku
 import os
 
-from .local_settings import SECRET_KEY
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
